b799e14a14268a69c450d9950be264008071654e
Matej Turek <469107@fi.muni.cz>
2021-11-19 18:46:01 +0100

    [ADD] Skeleton for quarkus application.

---
+++ narcos/src/test/java/cz/fi/muni/pv217/narcos/NativeGreetingResourceIT.java
@@ -0,0 +1,9 @@
+package cz.fi.muni.pv217.narcos;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeGreetingResourceIT extends GreetingResourceTest {
+
+    // Execute the same tests but in native mode.
+}
