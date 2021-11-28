3f761a52dd11ba0997d311f7e07b5956800a9bce
Matej Turek <469107@fi.muni.cz>
2021-11-28 18:46:52 +0100

    [FIX]

--- narcos/src/test/java/cz/fi/muni/pv217/narcos/NativeGreetingResourceIT.java
+++
@@ -1,9 +0,0 @@
-package cz.fi.muni.pv217.narcos;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeGreetingResourceIT extends GreetingResourceTest {
-
-    // Execute the same tests but in native mode.
-}
