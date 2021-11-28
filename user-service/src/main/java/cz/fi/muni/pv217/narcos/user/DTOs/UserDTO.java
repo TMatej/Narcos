70938db91f612cbc641927a8ba295d41add69d25
Matej Turek <469107@fi.muni.cz>
2021-11-28 18:51:51 +0100

    [ADD] DTO objects for logging in and passing out user information

---
+++ user-service/src/main/java/cz/fi/muni/pv217/narcos/user/DTOs/UserDTO.java
@@ -0,0 +1,13 @@
+package cz.fi.muni.pv217.narcos.user.DTOs;
+
+/**
+ * DTO representation of Person.
+ *
+ * @author Matej Turek
+ */
+public class UserDTO {
+    public Long id;
+    public String firstname;
+    public String lastName;
+    public String email;
+}
