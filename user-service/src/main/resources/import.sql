14842182530fe2d78b76f0f04c12245d00340a01
Matej Turek <469107@fi.muni.cz>
2021-12-05 14:10:14 +0100

    [FIX] Elements renamed according their roles

--- user-service/src/main/resources/import.sql
+++ user-service/src/main/resources/import.sql
@@ -1,3 +1,3 @@
-INSERT INTO persons (id, firstname, lastname, email, password, role, stored) VALUES (1, 'example', 'domain', 'example@domain.com', 'password', 0, true);
-INSERT INTO persons (id, firstname, lastname, email, password, role, stored) VALUES (2, 'Hannah', 'Backer', 'hannah.backer@domain.com', 'password123', 1, true);
+INSERT INTO persons (id, firstname, lastname, email, password, role, stored) VALUES (1, 'admin', 'localhost', 'admin@localhost.com', 'password', 0, true);
+INSERT INTO persons (id, firstname, lastname, email, password, role, stored) VALUES (2, 'user', 'localhost', 'user@localhost.com', 'password', 1, true);
 ALTER SEQUENCE hibernate_sequence RESTART WITH 3;
