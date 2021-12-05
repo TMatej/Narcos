5ebc7eb38e25762d3ae7088573a07abd9befe097
Matej Turek <469107@fi.muni.cz>
2021-12-05 22:16:35 +0100

    [FIX] Adjustments of scripts for db populations (create table if not exists introduced)

--- user-service/src/main/resources/import.sql
+++ user-service/src/main/resources/import.sql
@@ -4,7 +4,8 @@ CREATE TABLE IF NOT EXISTS person (
     lastname VARCHAR ( 256 ) NOT NULL,
     email VARCHAR ( 256 ) NOT NULL,
     password VARCHAR ( 256 ) NOT NULL,
-    role SMALLINT
+    role SMALLINT,
+    stored boolean
 );
 INSERT INTO person (id, firstname, lastname, email, password, role, stored) VALUES (1, 'admin', 'localhost', 'admin@localhost.com', 'password', 0, true);
 INSERT INTO person (id, firstname, lastname, email, password, role, stored) VALUES (2, 'user', 'localhost', 'user@localhost.com', 'password', 1, true);
