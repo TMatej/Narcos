e26c81c52a22eb6fef8165740f6880db15a23949
Matej Turek <469107@fi.muni.cz>
2021-12-05 13:53:24 +0100

    [ADD] Health (liveness and startup) for medicine service implemented

---
+++ medicine-service/src/main/java/cz/fi/muni/pv217/narcos/medicine/repository/MedicineRepository.java
@@ -0,0 +1,11 @@
+package cz.fi.muni.pv217.narcos.medicine.repository;
+
+import cz.fi.muni.pv217.narcos.medicine.entity.Medicine;
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+
+/**
+ * @author Matej Turek
+ */
+public class MedicineRepository implements PanacheRepository<Medicine> {
+
+}
