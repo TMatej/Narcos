730dc7d8c6d1e26fe4b657904f65b7a321c46ab5
Ondrej Machala <ondra097@seznam.cz>
2021-12-06 18:24:45 +0100

    wip communication between services

--- orders-service/src/main/java/cz/muni/fi/pv217/narcos/orders/OrdersResource.java
+++ orders-service/src/main/java/cz/muni/fi/pv217/narcos/orders/OrdersResource.java
@@ -3,11 +3,17 @@ package cz.muni.fi.pv217.narcos.orders;
 import cz.muni.fi.pv217.narcos.orders.dtos.CreateOrderItemDto;
 import cz.muni.fi.pv217.narcos.orders.dtos.OrderDto;
 import cz.muni.fi.pv217.narcos.orders.dtos.OrderItemDto;
+import cz.muni.fi.pv217.narcos.orders.dtos.UserDto;
 import cz.muni.fi.pv217.narcos.orders.entities.Order;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.eclipse.microprofile.jwt.JsonWebToken;
 import org.eclipse.microprofile.metrics.MetricUnits;
 import org.eclipse.microprofile.metrics.annotation.Counted;
 import org.eclipse.microprofile.metrics.annotation.Timed;
 
+import javax.annotation.security.PermitAll;
+import javax.annotation.security.RolesAllowed;
+import javax.inject.Inject;
 import javax.transaction.Transactional;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
@@ -17,8 +23,12 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.core.GenericType;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriBuilder;
+
 import cz.muni.fi.pv217.narcos.orders.dtos.CreateOrderDto;
 import cz.muni.fi.pv217.narcos.orders.entities.OrderItem;
 import cz.muni.fi.pv217.narcos.orders.entities.OrderStatus;
@@ -36,13 +46,14 @@ import java.util.stream.Collectors;
 public class OrdersResource {
     private static final Logger LOG = Logger.getLogger(OrdersResource.class);
 
-//    @ConfigProperty(name = "pv217.userService")
-//    String userServiceUrl;
-//
-//    @Inject
-//    JsonWebToken jwt;
+    @ConfigProperty(name = "pv217.userService")
+    String userServiceUrl;
+
+    @Inject
+    JsonWebToken jwt;
 
     @GET
+    @RolesAllowed({"Admin", "User"})
     public List<OrderDto> getAllOrders(@QueryParam("userId") Long userId) {
         LOG.info("Getting all orders" + (userId != null ? String.format(" from user %d", userId) : ""));
         List<Order> orders;
@@ -56,6 +67,7 @@ public class OrdersResource {
     }
 
     @GET
+    @RolesAllowed({"Admin", "User"})
     @Path("{id}")
     public Response getOrderById(@PathParam("id") Long id) {
         Optional<Order> maybeOrder = Order.findByIdOptional(id);
@@ -64,6 +76,7 @@ public class OrdersResource {
     }
 
     @POST
+    @RolesAllowed({"Admin", "User"})
     @Transactional
     @Path("new")
     @Counted(name = "createdOrders", description = "How many orders were created.")
@@ -86,6 +99,7 @@ public class OrdersResource {
     }
 
     @PUT
+    @RolesAllowed({"Admin", "User"})
     @Transactional
     @Path("{id}")
     public Response updateStatus(@PathParam("id") Long id, OrderStatus status) {
@@ -98,14 +112,15 @@ public class OrdersResource {
     }
 
     private boolean checkUserExists(Long userId) {
-        return true;
-//        UserDto user = ClientBuilder.newClient()
-//                .target(userServiceUrl + "/" + userId)
-//                .request()
-//                .header("Authorization", "Bearer " + jwt.getRawToken())
-//                .get()
-//                .readEntity(new GenericType<>(){});
-//        return user != null;
+        LOG.info(String.format("Jwt token: %s", jwt.getRawToken()));
+        LOG.info(String.format("Adress: %s", userServiceUrl + "/users/" + userId));
+        UserDto user = ClientBuilder.newClient()
+                .target( userServiceUrl + "/users/" + userId)
+                .request()
+                .header("Authorization", "Bearer " + jwt.getRawToken())
+                .get()
+                .readEntity(new GenericType<>(){});
+        return user != null;
     }
 
     private Order createOrderToEntity(CreateOrderDto order) {
