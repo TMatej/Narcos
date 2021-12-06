21e22b9a165533e75330871edc21ca8b33004c48
Matej Turek <80112137+TMatej@users.noreply.github.com>
2021-12-06 15:17:10 +0100

    Features... we need developers, developers, DEVELOPERS (#11)
    * [FIX] Adjustments of properties (mostly http.port values) and configurinng dev env.
    
    * [FIX] Adjustments of scripts for db populations (create table if not exists introduced)
    
    * [FIX] Correct public key inserted
    
    * [ADD] Medicine add to docker-compose
    
    * [ADD] Readiness for db connection added.
    
    * [FIX] Renaming of health check
    
    Co-authored-by: Matej Turek <469107@fi.muni.cz>

--- pharmacy-service/src/main/resources/import.sql
+++ pharmacy-service/src/main/resources/import.sql
@@ -1,2 +1,10 @@
-INSERT INTO pharmacy (name, street, streetNumber, city) VALUES ("Main", 1, "Brno")
-INSERT INTO pharmacy (name, street, streetNumber, city) VALUES ("Botanic", 26, "Kosice")
+CREATE TABLE IF NOT EXISTS pharmacy (
+    id serial PRIMARY KEY,
+    name VARCHAR ( 256 ) NOT NULL,
+    street VARCHAR ( 256 ) NOT NULL,
+    streetNumber SMALLINT NOT NULL,
+    city VARCHAR ( 256 ) NOT NULL
+);
+INSERT INTO pharmacy (id, name, street, streetNumber, city) VALUES (0, 'Doctor MAX', 'Main', 1, 'Brno');
+INSERT INTO pharmacy (id, name, street, streetNumber, city) VALUES (1, 'Na rožku', 'Botanic', 26, 'Kosice');
+ALTER SEQUENCE hibernate_sequence RESTART WITH 3;
